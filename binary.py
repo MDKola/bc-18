7528a0b9f2d928507b342884140ab579833bb4a0
kolaempire <denokolas@gmail.com>
2017-05-11 06:42:35 +0300

    implementation of the search method in progress

--- binary.py
+++ binary.py
@@ -11,3 +11,23 @@ class BinarySearch(list):
 
 	def search(self,val):
 		#performs a binary search to locate value
+		first = 0
+		last = len(self) - 1
+		value_index = 0
+		found = False
+
+		counter = 0
+		#counter initialized to zero
+
+		#checking position of value
+		if val==self[first]:
+			value_index = first
+			found = True
+		elif val == self[last]:
+			value_index = last
+			found = True
+
+		#making sure val is in list
+		if val not in self:
+			found = True
+			value_index = -1
